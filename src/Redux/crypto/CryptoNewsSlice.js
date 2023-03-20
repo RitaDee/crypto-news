b016851ac282a53a5958576829773674cd54e552
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-20 14:51:44 +0100

    Update cryptoNewsSlice.js file

--- src/Redux/crypto/CryptoNewsSlice.js
+++ src/Redux/crypto/CryptoNewsSlice.js
@@ -2,12 +2,32 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const url = 'https://financialmodelingprep.com/api/v4/crypto_news?page=0&apikey=68cda1598b1c39d73673af76bd5d5f33';
 
-export const fetchCryptoNews = createAsyncThunk('cryptoNews/fetch', async () => {
+const initialState = {
+  list: [],
+  isFetching: false,
+};
+
+export const fetchCryptoNews = createAsyncThunk('crypto/fetch', async () => {
   const response = fetch(url);
   const data = await response.json();
   return data;
 });
 
 const cryptoSlice = createSlice({
-    
-})
+  name: 'crypto',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCryptoNews.fulfilled, (state, action) => ({
+        ...state,
+        isFetching: false,
+        list: action.payload,
+      }))
+      .addCase(fetchCryptoNews.pending, (state) => ({
+        ...state,
+        isFetching: true,
+      }));
+  },
+});
+
+export default cryptoSlice.reducer;

9e1b8a0f2237a4f5b1ed27df173eb9f1b34dc47f
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-20 15:04:30 +0100

    Add Navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  {
+    to: '/',
+    text: 'Home',
+  },
+  {
+    to: '/details',
+    text: 'Details ',
+  },
+];
+
+export default links;

7d106a663f1a58b9e109c795f67be6adbc381865
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-20 15:29:41 +0100

    Update Home.js file

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
-import { fetchCryptoNews } from '../Redux/crypto/CryptoNewsSlice';
+// import { fetchCryptoNews } from '../Redux/crypto/CryptoNewsSlice';
+
+const Home = () => (
+  <h1>Hello Hello</h1>
+);
+
+export default Home;

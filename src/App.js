3fd339c255ac005409ea91a2503b958fbd165afd
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-21 14:28:41 +0100

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Details from './routes/Details';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/details" element={<Details />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
       <Outlet />
     </>

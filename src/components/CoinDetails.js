2c9e98f4520b3dd2877c467703209b4335d3c6ba
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-21 15:40:33 +0100

    Update CoinDetails.js file

--- src/components/CoinDetails.js
+++ src/components/CoinDetails.js
@@ -10,16 +10,18 @@ const CoinDetails = () => {
         <h1>Crypto Market Prices</h1>
       </div>
       <div className="coinsDetails">
-        <coinDetailsUI
-        name={coins.name}
-        currentPrice={coins.market_data.current_price.usd}
-        Alltimehigh={coins.market_data.ath.usd}
-        Alltimelow={coins.market_data.atl.usd}
+        <coinsDetailsUi
+          currentPrice={coins.market_data.current_price.usd}
+          Alltimehigh={coins.market_data.ath.usd}
+          Alltimelow={coins.market_data.atl.usd}
+          fullyDilutedValuation={coins.market_data.fully_diluted_valuation.usd}
+          marketCap={coins.market_data.market_cap.usd}
+          totalVolume={coins.market_data.total_volume}
+          priceChangePerDay={coins.market_data.price_change_24h.usd}
         />
       </div>
     </div>
   );
 };
-
 
 export default CoinDetails;

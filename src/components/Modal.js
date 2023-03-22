7edbbb2da76eca51537f42d44a2a98fcf2098742
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 09:37:07 +0100

    Update Modal.js file

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { RiCloseLine } from 'react-icons/ri';
+import { useDispatch } from 'react-redux';
+import coin
+
+
+const Modal = () => {
+  return (
+    <div>Modal</div>
+  )
+}
+
+export default Modal

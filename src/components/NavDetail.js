57542cbef0fa52b70aef0157a065c0b8ee09b389
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 09:16:14 +0100

    Install npm i -S react-icons

---
+++ src/components/NavDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdArrowBackIos } from 'react-icons/md';
+
+const NavDetail = () => (
+  <header>
+    <NavLink to="/" className="icondiv">
+      <MdArrowBackIos className="icon2" />
+    </NavLink>
+    <span>Crypto Sage</span>
+  </header>
+);
+
+export default NavDetail;

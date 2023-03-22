bc1d740ff0536f73ac3640a476a1ab577ff44af5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 09:10:02 +0100

    Fix linter errors

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -4,26 +4,25 @@ import CoinUi from './CoinUi';
 
 const CoinList = () => {
   const coins = useSelector((store) => store.coins.list);
-};
 
-return (
-  <div className='coinDiv'>
-    <div className='conHeading'>
-      <h2>Crypto Market Prices</h2>
+  return (
+    <div className="coinDiv">
+      <div className="conHeading">
+        <h2>Crypto Market Prices</h2>
+      </div>
+      <div>
+        {coins.map((coins) => (
+          <CoinUi
+            id={coins.id}
+            key={coins.id}
+            name={coins.name}
+            image={coins.image}
+            currentPrice={coins.current_price}
+          />
+        ))}
+      </div>
     </div>
-    <div>
-      {coins.map((coins) => (
-        <CoinUi
-        id={coins.id}
-        key={coins.id}
-        name={coins.name}
-        image={coins.image}
-        currentPrice={coins.current_price}
-        />
-      ))
-      }
-    </div>
-  </div>
-  )
+  );
+};
 
 export default CoinList;

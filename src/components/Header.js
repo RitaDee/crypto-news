959b5e371c9ee6d115a1aa5e3c640854ae287d37
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 09:32:40 +0100

    Update Header.js file

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="headerdiv">
+    {/* <img alt="cryptocurrencies" src={allcoins} className="imgheader" /> */}
+    <div className="subdiv">
+      <h1 className="headerh1">CRYPTO MARKET VALUE</h1>
+      <p className="headerh2">100 coins</p>
+    </div>
+  </div>
+);
+
+export default Header;

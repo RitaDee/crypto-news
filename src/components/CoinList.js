7b4f5c54ba1e6cf91061a0aacdda4babe49a9045
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 09:05:47 +0100

    Update CoinList.js file

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import CoinUi from './CoinUi';
 
-const CoinList = () => (
+const CoinList = () => {
   const coins = useSelector((store) => store.coins.list);
-  <div>CoinList</div>
-);
+};
+
+return (
+  <div className='coinDiv'>
+    <div className='conHeading'>
+      <h2>Crypto Market Prices</h2>
+    </div>
+    <div>
+      {coins.map((coins) => (
+        <CoinUi
+        id={coins.id}
+        key={coins.id}
+        name={coins.name}
+        image={coins.image}
+        />
+      ))
+      }
+    </div>
+  </div>
+  )
 
 export default CoinList;

639925456c6f0b1c449987fc8bb1d85e108a1d47
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 10:54:20 +0100

    Fix stylelint errors

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCoins } from '../Redux/coins/CoinsSlice';
+import { fetchCoinsDetail } from '../Redux/details/CoinsDetailsSlice';
+import CoinList from '../components/CoinList';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
-// import { fetchCryptoNews } from '../Redux/crypto/CryptoNewsSlice';
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCoins);
+    dispatch(fetchCoinsDetail('bitcoin'));
+  }, [dispatch]);
 
-const Home = () => (
-  <h1>Hello Micronauts</h1>
-);
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <CoinList />
+    </>
+  );
+};
 
 export default Home;

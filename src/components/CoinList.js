6cfc51bb2bba0f57e53c6836e0f346adb7ff75e5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-22 08:53:58 +0100

    Import useSelector hook

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import CoinUi from './CoinUi';
 
 const CoinList = () => (
+  const coins = useSelector((store) => store.coins.list);
   <div>CoinList</div>
 );
 

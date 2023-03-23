dffaf7d5fe4b924caec328abe3f7bb516f3b38fa
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-23 07:54:33 +0100

    update  CoinDetails.js component

--- src/components/CoinDetailsUi.js
+++ src/components/CoinDetailsUi.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../styles/CoinDetailsUi.module.css';
+import '../styles/CoinDetailsUi.css';
 
 const CoinDetailsUi = ({
   coinIcon,

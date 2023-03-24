0186924a33b6beb74d290a80a5247ca901047db6
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-24 13:13:28 +0100

    Write CoinsDetailSlice comnponent test

--- src/__tests__/Redux/CoinsDetails.test.js
+++ src/__tests__/Redux/CoinsDetails.test.js
@@ -1,1 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+import store from '../../Redux/store';
+import CoinsDetailsSlice from '../../Redux/details/CoinsDetailsSlice';
+
+function MyFallbackComponent({ error, resetErrorBoundary }) {
+  return (
+    <div>
+      <h2>Something went wrong:</h2>
+      <p>{error.message}</p>
+      <button type="button" onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+describe('CoinsDetailsSlice', () => {
+  it('renders correctly', () => {
+    const tree = renderer(
+      <ErrorBoundary FallbackComponent={MyFallbackComponent}>
+        <Provider store={store}>
+          <CoinsDetailsSlice />
+        </Provider>
+      </ErrorBoundary>,
+    )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+MyFallbackComponent.propTypes = {
+  resetErrorBoundary: PropTypes.func.isRequired,
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }).isRequired,
+};

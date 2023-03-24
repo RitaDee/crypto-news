323fa0dc074e0247a999c1fe4c59fc063a88fcdd
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-24 12:10:22 +0100

    Write CoinLis comnponent test

--- src/__tests__/components/CoinList.test.js
+++ src/__tests__/components/CoinList.test.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../../Redux/store';
+import CoinList from '../../components/CoinList';
 
+describe('CoinList', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <CoinList />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
